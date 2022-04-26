e7151adff7ba1f652a54e783e75d154b96201239
teja0909 <69634961+teja0909@users.noreply.github.com>
2022-04-26 21:38:29 +0530

    Add files via upload

---
+++ nearest_smaller_to_right.py
@@ -0,0 +1,14 @@
+stack=[]
+op=[]
+a=[int(i) for i in input().split()]
+for i in a[::-1]:
+    if len(stack)==0:op.append(-1)
+    else:
+        if i>stack[-1]:op.append(stack[-1])
+        else:
+            while len(stack)>0 and stack[-1]>i:
+                stack.pop()
+            if len(stack)==0:op.append(-1)
+            else:op.append(stack[-1])
+    stack.append(i)
+print(*op[::-1])
