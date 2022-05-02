0ac23fc9f92638523e902c319e383ed6bc08514f
teja0909 <69634961+teja0909@users.noreply.github.com>
2022-05-02 12:37:38 +0530

    Create Rotate_Array.py

---
+++ leetcode/Rotate_Array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
+"""
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0,nums[-1])
+            l=nums.pop()
+ """
